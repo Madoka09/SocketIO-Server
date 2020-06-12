998c757d7fded523dc6077ca65ce3bf080d1d859
unknown <shippuuga.zero@gmail.com>
2020-06-12 17:30:43 -0500

    V3

--- index.js
+++ index.js
@@ -1,26 +1,79 @@
 let app = require('express');
-
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
-
 const server = app()
     .use((req, res) => res.sendFile(INDEX, {root: __dirname}))
     .listen(PORT, () => console.log(`En puerto ${PORT}`))
 
+const { userJoin, getCurrentUser, userLeave, getRoomUsers, getAllRooms, roomLeave } = require ('./utils/users');
 let io = require('socket.io')(server);
+
+
 
 io.on('connection', (socket) => {
 
+    console.log('new ws connection');
+
+    // when client from table connects
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+        socket.join(user.room);
+
+        // send connect event
+        io.to(user.room).emit('users-changed', { user: user.username, event: 'joined' });
+
+        //send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+
+        socket.broadcast.emit('broadcast', user.room)
+    })
+
+
+    // when client disconnects
     socket.on('disconnect', function () {
-        io.emit('users-changed', { user: socket.username, event: 'left' });
-    });
+        const user = userLeave(socket.id);
 
-    socket.on('set-name', (name) => {
-        socket.username = name;
-        io.emit('users-changed', { user: name, event: 'joined' });
-    });
+        if(user){
+            // send disconnect event
+            io.to(user.room).emit('users-changed', { user: user.username, event: 'left' });
+            console.log(`desconectado de ${getAllRooms()}`)
+            socket.broadcast.emit('broadcast', user.room)
 
+            //send users and room info
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            })
+
+            roomLeave(user.room);
+        }
+
+
+    });   
+    
+    // when sending message
     socket.on('send-message', (message) => {
-        io.emit('message', { msg: message.text, user: socket.username, createdAt: new Date() });
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('message', { msg: message.text, user: user.username, createdAt: new Date() });
+        console.log(`sending message to ${user.room}`)
     });
+
+    // send message from waiter
+    socket.on('waiter-message', (message) => {
+        const user = getCurrentUser(socket.id);
+
+        io.to(message.room).emit('message', { msg: message.text, user: user.username, createdAt: new Date() })
+        console.log(`cuarto del mesero ${message.room}`)
+        console.log(`Dejando cuarto ${user.room}`)
+    })
+
+    socket.on('getRooms', function() {
+       io.emit('getRooms', getAllRooms())
+       console.log(getAllRooms())
+    })
+
 });
---
+++ utils/users.js
@@ -0,0 +1,58 @@
+const e = require("express");
+
+const users = [];
+const rooms = [];
+// Join user to chat
+function userJoin(id, username, room) {
+    const user = { id, username, room};
+
+    if(rooms.indexOf(room) === -1 ){
+        rooms.push(room);
+    }
+
+    users.push(user);
+
+    return user;
+}
+
+// Get current User
+function getCurrentUser(id){
+    return users.find(user => user.id === id);
+}
+
+// Get room users
+function getRoomUsers(room){
+    return users.filter(user => user.room === room);
+}
+
+// Get all rooms
+function getAllRooms(){
+    return rooms;
+}
+
+// Room leave
+function roomLeave(room){
+    const index = rooms.indexOf(room)
+
+    if(index !== -1 ){
+        return rooms.splice(index, 1)[0];
+    }
+}
+
+// User leaves
+function userLeave(id) {
+    const index = users.findIndex(user => user.id === id);
+
+    if (index !== -1 ){
+        return users.splice(index, 1)[0];
+    }
+}
+
+module.exports = {
+    userJoin,
+    getCurrentUser,
+    getRoomUsers,
+    userLeave,
+    getAllRooms,
+    roomLeave
+};
